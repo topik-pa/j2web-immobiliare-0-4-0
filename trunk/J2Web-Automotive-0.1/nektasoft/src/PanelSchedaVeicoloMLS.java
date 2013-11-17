e22016d8e24cb23e8eeab5c8b37f1b87d6225f75
marcopavan.mp@gmail.com <marcopavan.mp@gmail.com@298d9817-40e6-16c5-61fe-d4d9b89b0411>
2013-11-17 16:44:10 +0000

    Creazione branch nektasoft

---
+++ trunk/J2Web-Automotive-0.1/nektasoft/src/PanelSchedaVeicoloMLS.java
@@ -0,0 +1,139 @@
+//Questa classe definisce tutti i sottopannelli schede veicolo
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.swing.JComponent;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+
+class PanelSchedaVeicoloMLS extends JPanel {   
+
+	JPanel pannelloListaPortali = J2Web_UI.getPanel_10();
+
+	private static final long serialVersionUID = 1L;
+
+	SchedaVeicolo scheda;
+	Long idScheda;
+	String codiceScheda;
+	JButton btnCancellaScheda;
+	JButton btnEsportaScheda;
+	JRadioButton schedaRadio;
+
+	String labelSpaziatore = "   "; 
+
+	public PanelSchedaVeicoloMLS(final SchedaVeicolo scheda) {
+		this.scheda = scheda;
+		idScheda = scheda.idScheda;
+		codiceScheda = scheda.codiceScheda;
+
+		setLayout(new BorderLayout(0, 0));
+		setBorder(new LineBorder(Color.LIGHT_GRAY));
+		setMaximumSize(new Dimension(600, 130));
+
+		//Radio button dei sottopannelli
+		schedaRadio = new JRadioButton("Seleziona scheda");
+		//Le radio button devono appartenere allo stesso gruppo per funzionare correttamente
+		//radioGrpSchede.add(schedaRadio); 
+		//Clicco su una radio button di una scheda
+		schedaRadio.addActionListener(new ActionListener() {			 
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("Scheda selezionata: " + scheda.codiceScheda);          
+
+				Component[] wrapperScheda = getParent().getComponents();
+				for(int i=0; i<wrapperScheda.length; i++) {
+					if(wrapperScheda[i].getClass().toString().contains("PanelSchedaVeicolo"))  {
+						((JComponent) wrapperScheda[i]).setBorder(new LineBorder(Color.LIGHT_GRAY));
+					}
+				}
+
+				setBorder(new LineBorder(Color.ORANGE));
+
+				mostraDettagliVeicoloMLS(scheda);
+			}
+		});
+		add(schedaRadio, BorderLayout.NORTH);
+
+		//La label delle schede
+		String labelScheda = scheda.marcaVeicolo + " " + scheda.modelloVeicolo + " " + scheda.versioneVeicolo + " - " + scheda.tipologiaVeicolo + " " + scheda.coloreEsternoVeicolo + " " + scheda.prezzoVeicolo + " - " + scheda.ragioneSociale + " " + scheda.nomeReferente;
+		String tooltipScheda = labelScheda;
+		if(labelScheda.length()>60) {	//è molto probabile che lo sia... :)
+			labelScheda = labelScheda.substring(0, 60); 
+		}		 
+		labelScheda+="...";
+		JLabel label = new JLabel(labelScheda);
+		Font font = new Font("Monospaced", Font.PLAIN, 11);
+		label.setFont(font);
+		label.setHorizontalTextPosition(SwingConstants.LEFT);
+
+		BufferedImage imgtest = null;
+		if(scheda.arrayImages[1]!=null) {
+			try {
+				imgtest = ImageIO.read(scheda.arrayImages[1]);
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			Image resizedimg = imgtest.getScaledInstance(70, 50, Image.SCALE_FAST);          
+
+			label.setIcon(new ImageIcon(resizedimg));
+		}
+
+		add(label, BorderLayout.CENTER);
+
+		//Aggiungo una tooltip
+		setToolTipText(tooltipScheda);		 
+
+	}
+
+	private void  mostraDettagliVeicoloMLS(SchedaVeicolo schedaVeicolo) {
+
+		JPanel pannelloInfoVeicoloMLS = J2Web_UI.getPanel_8();
+
+		pannelloInfoVeicoloMLS.removeAll();
+
+		System.out.println("Info veicolo selezionato...");
+
+		String[][] matrix = new String[1][5];
+
+		for (int row = 0; row < matrix.length; row++) {
+			// for (int column = 0; column < matrix[row].length; column++)
+			matrix[row][0] = schedaVeicolo.marcaVeicolo;
+			matrix[row][1] = schedaVeicolo.modelloVeicolo;
+			matrix[row][2] = schedaVeicolo.versioneVeicolo;
+			matrix[row][3] = schedaVeicolo.coloreEsternoVeicolo;
+			matrix[row][4] = schedaVeicolo.chilometraggioVeicolo;
+		}
+
+		JTable table = new JTable();
+		table.setModel(new DefaultTableModel(
+				matrix,
+				new String[] {
+						"Marca", "Modello", "Versione", "Colore", "Chilometri"
+				}
+				));
+		
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		table.setFillsViewportHeight(true);
+		
+		pannelloInfoVeicoloMLS.add(table.getTableHeader(), BorderLayout.PAGE_START);
+		pannelloInfoVeicoloMLS.add(table, BorderLayout.CENTER);
+	}
+
+}
---
+++ trunk/J2Web-Automotive-0.1/nektasoft/src/PortaleWeb.java
@@ -0,0 +1,286 @@
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.http.Header;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+import org.jsoup.nodes.Element;
+import java.util.Iterator;
+
+
+
+
+/*
+ * Questa classe definische i metodi e gli attributi dell'oggetto portale web, qui definiti in termini generici, vengono riscritti nelle classi più specifiche
+ *
+ */
+
+
+/**
+ *
+ * @author marco - marcopavan.mp@gmail.com 
+ */
+
+public abstract class PortaleWeb implements parametriGenerali {
+
+	//Attributi
+	ImageIcon icon;
+	String valoreLabel;
+	String idPortale;
+	boolean isActive;	
+
+	//Cookie
+	protected static String SESSIONCOOKIEHEADER;
+	protected static String SESSIONCOOKIENAME;
+	protected static String SESSIONCOOKIEVALUE;
+	protected static String SESSIONCOOKIEDOMAIN;
+
+	//La label che identifica i parametri da non inviare
+	protected static String dontSendThisParam = "***DONOTSEND***";
+
+	public PortaleWeb (ImageIcon icon, String valoreLabel, String idPortale, boolean isActive) {
+
+		this.icon = icon;
+		this.valoreLabel = valoreLabel;
+		this.idPortale = idPortale;
+		this.isActive = isActive;
+
+	}
+
+
+	//Metodi
+
+	//Inserimento scheda (sovrascritto nelle sottoclassi)
+	public abstract boolean inserisciScheda(SchedaVeicolo scheda, boolean isSequential) throws HttpCommunicationException, UnsupportedEncodingException;
+
+	//Visualizzazione scheda (sovrascritto nelle sottoclassi)
+	public abstract boolean visualizzaScheda(SchedaVeicolo scheda) throws HttpCommunicationException;
+
+	//Eliminazione scheda (sovrascritto nelle sottoclassi)
+	public abstract boolean cancellaScheda(SchedaVeicolo scheda, boolean isSequential) throws HttpCommunicationException;
+
+	//Invio mail di conferma inserzione
+	static void sendConfirmationMail(SchedaVeicolo scheda, String nomePortale, String codInserzione)   {
+
+		final String USERNAME = BACKEND_EMAIL;
+		final String PASSWORD = BACKEND_EMAIL_PSW;
+		final String RECIPENTS = BACKEND_EMAIL + "," + EMAIL_UTENTE;
+		final String SUBJECT = "Scheda inserita: " + scheda.codiceScheda + " " + scheda.marcaVeicolo + " " + scheda.modelloVeicolo + " " + scheda.versioneVeicolo ;
+
+		final String SMTP_HOST = BACKEND_EMAIL_SMTP_HOST;
+		final int SMTP_PORT = BACKEND_EMAIL_SMTP_PORT;
+		final Session session = Session.getInstance(System.getProperties(), null);
+		final Message msg = new MimeMessage(session);
+		final String senderEmail = USERNAME.contains("@") ? USERNAME : (USERNAME + BACKEND_EMAIL_DOMAIN);
+
+		String textBody = "";	  	
+		textBody += "E' stata inserita una scheda nel portale: " + nomePortale + ".";
+		textBody += "Il codice della scheda è: \n" + scheda.codiceScheda + "/n" + "Il codice di inserimento è: /n" + codInserzione;
+
+
+		try {
+			msg.setFrom(new InternetAddress(senderEmail));
+			final Address[] recipientAddresses = InternetAddress.parse(RECIPENTS);
+			msg.setRecipients(Message.RecipientType.TO, recipientAddresses);
+
+			msg.setSentDate(new Date());
+			msg.setSubject(SUBJECT);
+
+			msg.setText("Dati scheda: \n\n " + textBody);
+
+			final Transport transport = session.getTransport("smtps");
+			transport.connect(SMTP_HOST, SMTP_PORT, USERNAME, PASSWORD);
+			transport.sendMessage(msg, recipientAddresses);
+			transport.close();
+
+		} catch (AddressException e) {
+			JOptionPane.showMessageDialog(null, MapModalWindowsDialogs.get("sendConfirmationMail"), "AddressException", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			JOptionPane.showMessageDialog(null, MapModalWindowsDialogs.get("sendConfirmationMail"), "MessagingException", JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+		}
+	}
+
+	//Valutazione similarità tra stringhe
+	public static List<char[]> bigram(String input)
+	{
+		ArrayList<char[]> bigram = new ArrayList<char[]>();
+		for (int i = 0; i < input.length() - 1; i++)
+		{
+			char[] chars = new char[2];
+			chars[0] = input.charAt(i);
+			chars[1] = input.charAt(i+1);
+			bigram.add(chars);
+		}
+		return bigram;
+	}      
+	public static double dice(List<char[]> bigram1, List<char[]> bigram2)
+	{
+		List<char[]> copy = new ArrayList<char[]>(bigram2);
+		int matches = 0;
+		for (int i = bigram1.size(); --i >= 0;)
+		{
+			char[] bigram = bigram1.get(i);
+			for (int j = copy.size(); --j >= 0;)
+			{
+				char[] toMatch = copy.get(j);
+				if (bigram[0] == toMatch[0] && bigram[1] == toMatch[1])
+				{
+					copy.remove(j);
+					matches += 2;
+					break;
+				}
+			}
+		}
+		return (double) matches / (bigram1.size() + bigram2.size());
+	}
+
+	//Trova il cookie di sessione
+	public boolean findSessionCookie(Header[] headers, String cookieName, String cookieDomain) {
+
+		boolean cookieHeaderFound = false;
+
+		String cookie_header = "";
+		String cookie_value = "";
+
+		for(int i=0; i<headers.length; i++) {       	
+			Header currentHeader = headers[i];
+			//Get cookie
+			if(currentHeader.getName().contains("Set-Cookie")) {
+
+				cookie_header = currentHeader.getValue();
+				int end = cookie_header.indexOf("=");
+				String cookie_name = cookie_header.substring(0, end);                   
+				int start = end + 1;
+				end = cookie_header.indexOf(";");
+				cookie_value = cookie_header.substring(start, end);
+
+				if(cookie_name.equals(cookieName)) {
+					//Cookie di sessione trovato
+					cookieHeaderFound = true;            		
+
+					//Aggiorno i parametri dei cookie (del portale chiamante)
+					SESSIONCOOKIEHEADER = cookie_header;
+					SESSIONCOOKIEVALUE = cookie_value;
+					
+					//Stampo i valori trovati
+					System.out.println("Method: findSessionCookie \n" + "cookie_header-->"+cookie_header + "\ncookieName-->"+cookieName + "\ncookie_value-->"+cookie_value + "\ncookieDomain-->"+cookieDomain);
+				}   
+			}       	
+		}		
+
+		//Valori di ritorno
+		return cookieHeaderFound?true:false;
+	}
+
+	//Ritorna il valore di una header dato il nome dell'header stesso
+	public String getHeaderValueByName(Header[] headers, String headerName) {
+
+		String headerValue = "Header non trovato";
+
+		//boolean headerFound = false;
+		for(int i=0; i<headers.length; i++) {       	
+			Header currentHeader = headers[i];
+			//Get cookie
+			if(currentHeader.getName().contains(headerName)) {     		        		
+				headerValue = currentHeader.getValue();	
+				//Stampo i valori trovati
+				System.out.println("Method: getHeaderValueByName \n" + "headerValue-->" + headerValue);
+			}       	
+		}       
+		//Valore tornato
+		return headerValue;
+	}
+
+	//Metodo per ottenere le coordinate della città
+	public Map<String,String> getLatLonCoord(String indirizzo, String comune, String provincia, String regione) throws ParserConfigurationException, SAXException, IOException {
+		Map<String,String> mappaLatLon = new Hashtable<String,String>();
+		String url;
+		String latitudine;
+		String longitudine;
+		Document doc = null;
+		DocumentBuilder db = null;
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		NodeList nodelist;
+		Node latitude = null, longitude = null;
+
+		db = dbf.newDocumentBuilder();
+		url = "http://maps.googleapis.com/maps/api/geocode/xml?sensor=false&address=" + URLEncoder.encode(indirizzo, "UTF-8") + "%20" + URLEncoder.encode(comune, "UTF-8") + "%20" + URLEncoder.encode(provincia, "UTF-8") + "%20" + URLEncoder.encode(regione, "UTF-8") + "%20" + "Italia";	
+		doc = db.parse(url);
+
+		doc.getDocumentElement().normalize();
+		nodelist = doc.getElementsByTagName("location");
+		if(nodelist!=null) {
+			Node locationNode = nodelist.item(0);
+			NodeList locationNodeChilds = locationNode.getChildNodes();
+			latitude = locationNodeChilds.item(1);
+			longitude = locationNodeChilds.item(3);
+		} 
+		latitudine = latitude.getTextContent();
+		longitudine = longitude.getTextContent();
+
+		mappaLatLon.put("latitudine", latitudine);
+		mappaLatLon.put("longitudine", longitudine);
+
+		return mappaLatLon;       	
+	}
+
+	//Metodo per ottenere il valore di un input dato il nome dell'input stesso
+	public String getInputValueByName(String responseBody, String inputName) {
+		String inputValue = null;
+
+		org.jsoup.nodes.Document doc = Jsoup.parse(responseBody);
+		Elements inputElements = doc.getElementsByTag("input");
+		if(inputElements!=null) {
+			Iterator<Element> iterator = inputElements.iterator();
+			while(iterator.hasNext()) {
+				Element currentElement = iterator.next();
+				if(currentElement.attr("name").equals(inputName)) {
+					inputValue = currentElement.attr("value");
+				}
+			}
+		}
+		System.out.println("Method: getInputValueByName: " + inputName + " --> " + inputValue);
+		return inputValue; 
+	}
+
+	//Metodo per evitare l'invio di parametri
+	public List<NameValuePair> removeNotUsedParams(List<NameValuePair> paramList) {	
+
+		List<NameValuePair> cleanedList = paramList;
+
+		Iterator<NameValuePair> iterator = cleanedList.iterator();
+		while(iterator.hasNext()) {
+			BasicNameValuePair currentParam = (BasicNameValuePair) iterator.next();
+			if(currentParam.getValue() == dontSendThisParam)  {
+				iterator.remove();	
+			}
+		}
+		return cleanedList;
+	}
+}
