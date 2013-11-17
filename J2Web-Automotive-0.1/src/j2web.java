a2dd4b6cf1f55557b5f4702793ad6d37535287e0
marcopavan.mp@gmail.com <marcopavan.mp@gmail.com@298d9817-40e6-16c5-61fe-d4d9b89b0411>
2013-11-17 17:34:14 +0000

    rimesso i portali su cui lavoro + aggiunto logo cuboauto

--- J2Web-Automotive-0.1/src/j2web.java
+++ J2Web-Automotive-0.1/src/j2web.java
@@ -190,13 +190,13 @@ public class j2web implements parametriGenerali {
 		PortaleWeb _cuboAutoIt = new _cuboAutoIt(_cuboAutoIcon, "3 - cuboauto.it", "003", true);
 		J2Web_UI.listPortaliSincronizzazione.add(_cuboAutoIt);
 		
-		ImageIcon _subitoItIcon  = new ImageIcon(cl.getResource("images/_subitoIt.png"));
+		/*ImageIcon _subitoItIcon  = new ImageIcon(cl.getResource("images/_subitoIt.png"));
 		PortaleWeb _subitoIt = new _subitoIt(_subitoItIcon, "4 - subito.it", "004", true);
 		J2Web_UI.listPortaliSincronizzazione.add(_subitoIt);
 		
 		ImageIcon _kijijiItIcon  = new ImageIcon(cl.getResource("images/_kijijiIt.png"));
 		PortaleWeb _kijijiIt = new _kijijiIt(_kijijiItIcon, "5 - kijiji.it", "005", true);
-		J2Web_UI.listPortaliSincronizzazione.add(_kijijiIt);
+		J2Web_UI.listPortaliSincronizzazione.add(_kijijiIt);*/
 
 	}
 
