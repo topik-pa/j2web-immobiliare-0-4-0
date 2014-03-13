ec1a163eaa9108746ef264cccd387a90cae5f655
marcopavan.mp@gmail.com <marcopavan.mp@gmail.com@298d9817-40e6-16c5-61fe-d4d9b89b0411>
2014-03-13 17:40:19 +0000

    metto in manutenzione 2 portali

--- trunk/J2Web-Automotive-0.1/src/j2web.java
+++ trunk/J2Web-Automotive-0.1/src/j2web.java
@@ -195,7 +195,7 @@ public class j2web implements parametriGenerali {
 		J2Web_UI.listPortaliSincronizzazione.add(_portaleMLS);
 
 		ImageIcon _autoscout24ItIcon  = new ImageIcon(cl.getResource("images/_autoscout24It.png"));
-		PortaleWeb _autoscout24It = new _autoscout24It(_autoscout24ItIcon, "2 - autoscout24.it", "002", true);
+		PortaleWeb _autoscout24It = new _autoscout24It(_autoscout24ItIcon, "2 - autoscout24.it", "002", false);
 		J2Web_UI.listPortaliSincronizzazione.add(_autoscout24It);
 
 		ImageIcon _cuboAutoIcon  = new ImageIcon(cl.getResource("images/_cuboAutoIt.png"));
@@ -215,7 +215,7 @@ public class j2web implements parametriGenerali {
 		J2Web_UI.listPortaliSincronizzazione.add(_vendiautoCom);
 
 		ImageIcon _subitoItIcon  = new ImageIcon(cl.getResource("images/_subitoIt.png"));
-		PortaleWeb _subitoIt = new _subitoIt(_subitoItIcon, "6 - subito.it", "006", true);
+		PortaleWeb _subitoIt = new _subitoIt(_subitoItIcon, "6 - subito.it", "006", false);
 		J2Web_UI.listPortaliSincronizzazione.add(_subitoIt);
 		
 		ImageIcon _autoprontaconsegnaComIcon  = new ImageIcon(cl.getResource("images/_autoprontaconsegnaCom.png"));
